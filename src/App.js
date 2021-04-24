6d29034763c516097c1d6ab585b84f690277c974
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-24 09:32:58 -0700

    initalized State for contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import About from "./components/About";
 import "./App.css";
 import Header from "./components/Header";
+import Contact from "./components/Contact";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [content] = useState(["About", "Contact", "Portfolio", "Resume"]);
@@ -15,6 +17,7 @@ function App() {
         setCurrentContent={setCurrentContent}
       />
       <main>
+        <ContactForm></ContactForm>
         <About></About>
       </main>
     </div>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
   return (
     <section>
       <h1> Contact Me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" defaultValue={name} name="name" />
         </div>
         <div>
           <label htmlFor="email">Email Address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
