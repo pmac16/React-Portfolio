ce29ecbe95799be9b357c5784443332d7816b67e
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-25 11:53:20 -0700

    added more information to bio in about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,7 +17,7 @@ function About() {
         <img
           src={coverImage}
           className="my-2"
-          style={{ width: 200 }}
+          style={{ width: 400, height: 400 }}
           alt="cover"
         />
         <div
@@ -29,9 +29,25 @@ function About() {
           }}
         >
           <p>
-            My name is Priya Macpherson. I am a full stack web developer based
-            in Oakland, CA. Previously I was an elementary school teacher and
-            taught 5th grade.
+            From a young age, I have always loved learning. However, the passion
+            I had for learning did not extend to math, technology, or science.
+            When I became a teacher, you can imagine my surprise when I
+            discovered that though I had hated these subjects as a child, I
+            loved teaching them as an adult. I learned the creativity involved
+            with math; there is always more than one way to solve a problem. I
+            discovered female scientists I had never learned about and shared
+            their research discoveries with my students. I attended coding
+            professional developments so I could teach my students to code. And
+            through all of this, I discovered how deep my own love of this field
+            had become. I knew I needed to learn more.
+            <br></br> <br></br>I got into teaching because I wanted to make a
+            difference in my community. I wanted all of my students to have the
+            same academic outcomes as students in high-income areas. My shift
+            into computer coding comes from the same desire.
+            <br></br> <br></br>
+            Through my learnings from UC Berkeley's coding bootcamp, I plan on
+            continuing my work in ensuring that all students have access to
+            quality educational programming.
           </p>
         </div>
       </div>
