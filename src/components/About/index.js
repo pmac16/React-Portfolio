5abee67686108c3d2ce939474e32852817b7650e
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-25 11:45:13 -0700

    changed font and fixed About styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,18 +5,35 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">About Me</h1>
-      <img
-        src={coverImage}
-        className="my-2"
-        style={{ width: 200 }}
-        alt="cover"
-      />
-      <div className="my-2">
-        <p>
-          My name is Priya Macpherson. I am a full stack web developer based in
-          Oakland, CA. Previously I was an elementary school teacher and taught
-          5th grade.
-        </p>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          alignItems: "left",
+          justifyContent: "space-between",
+          margin: 10,
+        }}
+      >
+        <img
+          src={coverImage}
+          className="my-2"
+          style={{ width: 200 }}
+          alt="cover"
+        />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "left",
+            justifyContent: "space-between",
+            margin: 30,
+          }}
+        >
+          <p>
+            My name is Priya Macpherson. I am a full stack web developer based
+            in Oakland, CA. Previously I was an elementary school teacher and
+            taught 5th grade.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,17 @@ function Header(props) {
   const { content = [], setCurrentContent, currentContent } = props;
 
   return (
-    <header className="flex-row px-1">
+    <header
+      className="flex-row px-1"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
       <h2 className="mx-2">
         <a data-testid="link" href="/">
           Priya Macpherson
         </a>
       </h2>
       <nav>
-        <ul className="flex-row">
+        <ul className="flex-row" style={{ alignItems: "right" }}>
           <li className="mx-2"></li>
           {content.map((content) => (
             <li
